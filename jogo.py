51c179ef954c8be3b6e8840272d1ac975c477aa9
Rodrigo Exterckötter Tjäder <rodrigo@tjader.com.br>
2011-03-29 21:28:31 -0300

    Actor, minha gente.

--- jogo.py
+++ jogo.py
@@ -10,19 +10,10 @@ black = 0, 0, 0
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Aventuras de Ananias e Benevides') 
 
-ananias = pygame.image.load("imagens/s_ananias_baixo.png")
-ananias = pygame.transform.scale(ananias, [4 * i for i in ananias.get_rect()][2:]).convert_alpha()
-ananiasrect = ananias.get_rect()
-ananiasspeed = [0, 0]
-benevides = pygame.image.load("imagens/s_benevides_baixo.png")
-benevides = pygame.transform.scale(benevides, [4 * i for i in benevides.get_rect()][2:]).convert_alpha()
-benevidesrect = benevides.get_rect()
-benevidesspeed = [0, 0]
-corletra = 0
-pasto = pygame.image.load("imagens/t_grama.png")
+pasto = pygame.image.load("images/t_grama.png")
 pasto = pygame.transform.scale(pasto, [4 * i for i in pasto.get_rect()][2:]).convert_alpha()
 pastorect = pasto.get_rect()
-fro = pygame.image.load("imagens/t_gramaflor.png")
+fro = pygame.image.load("images/t_gramaflor.png")
 fro = pygame.transform.scale(fro, [4 * i for i in fro.get_rect()][2:]).convert_alpha()
 fundo = pygame.Surface(size)
 
@@ -33,66 +24,29 @@ for i in range(0, size[0], pastorect[2]):
 		else:
 			fundo.blit(pasto, (i, j))
 
+import actor
+ananias = actor.Actor('s_ananias_cima.png', (pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN))
+benevides = actor.Actor('s_benevides_cima.png', (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s))
+
 tempo = pygame.time.Clock()
-quadroimpar = False
 
 while True:
-	if quadroimpar:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
-			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_RIGHT:
-					ananiasspeed[0] += 1
-				if event.key == pygame.K_LEFT:
-					ananiasspeed[0] -= 1
-				if event.key == pygame.K_DOWN:
-					ananiasspeed[1] += 1
-				if event.key == pygame.K_UP:
-					ananiasspeed[1] -= 1
-				if event.key == pygame.K_d:
-					benevidesspeed[0] += 1
-				if event.key == pygame.K_a:
-					benevidesspeed[0] -= 1
-				if event.key == pygame.K_s:
-					benevidesspeed[1] += 1
-				if event.key == pygame.K_w:
-					benevidesspeed[1] -= 1
-				if event.key == pygame.K_q:
-					sys.exit()
-			elif event.type == pygame.KEYUP:
-				if event.key == pygame.K_RIGHT:
-					ananiasspeed[0] -= 1
-				if event.key == pygame.K_LEFT:
-					ananiasspeed[0] += 1
-				if event.key == pygame.K_DOWN:
-					ananiasspeed[1] -= 1
-				if event.key == pygame.K_UP:
-					ananiasspeed[1] += 1
-				if event.key == pygame.K_d:
-					benevidesspeed[0] -= 1
-				if event.key == pygame.K_a:
-					benevidesspeed[0] += 1
-				if event.key == pygame.K_s:
-					benevidesspeed[1] -= 1
-				if event.key == pygame.K_w:
-					benevidesspeed[1] += 1
-			else:
-				print event
+	events = pygame.event.get()
+	ananias.input(events)
+	benevides.input(events)
+	for event in events:
+		if event.type == pygame.QUIT \
+		   or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
+			sys.exit()
 
-	ananiasrect = ananiasrect.move([2 * i for i in ananiasspeed])
-	benevidesrect = benevidesrect.move([2 * i for i in benevidesspeed])
-	quadroimpar = not quadroimpar
+	ananias.update()
+	benevides.update()
 
-	if (ananiasrect.colliderect(benevidesrect)):
-		corletra = 255
 	screen.blit(fundo, (0, 0))
-	screen.blit(pygame.font.SysFont('Linux Libertine O', 36).render(unicode('Testefi', 'utf8'), True, (corletra, corletra, corletra)), (50, 50, 50, 50))
-	if corletra > 0:
-		corletra -= 1
-	screen.blit(ananias, ananiasrect)
-	screen.blit(benevides, benevidesrect)
+	ananias.render(screen)
+	benevides.render(screen)
 	pygame.display.flip()
+
 	tempo.tick(60)
 	print tempo.get_fps(), 'QPS'
 
