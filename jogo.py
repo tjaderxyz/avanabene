f80fafc2e275af281fb9ece5123c95347b16173c
Rodrigo Exterckötter Tjäder <rodrigo@tjader.com.br>
2011-03-30 01:01:59 -0300

    Música

--- jogo.py
+++ jogo.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # coding: utf-8
 
-import sys, pygame, random, datetime
+import os, sys, pygame, random, datetime
 pygame.init()
 
 size = width, height = 640, 480
@@ -33,8 +33,11 @@ ananias = actor.Actor('ananias', (pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, py
 benevides = actor.Actor('benevides', (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s))
 
 tela = [0, 0]
-txt = pygame.font.SysFont('Unifont', 16).render(repr(tela), False, (255, 255, 255))
+txt = pygame.font.SysFont('unifont', 16).render(repr(tela), False, (255, 255, 255))
 txt = pygame.transform.scale(txt, [4 * i for i in txt.get_rect()][2:])
+
+pygame.mixer.music.load(os.path.join('music', 'lojinha song.mp3'))
+pygame.mixer.music.play(-1)
 
 tempo = pygame.time.Clock()
 
@@ -75,7 +78,7 @@ while True:
 			return
 		global fundo, txt
 		fundo = gerafundo()
-		txt = pygame.font.SysFont('Unifont', 16).render(repr(tela), False, (255, 255, 255))
+		txt = pygame.font.SysFont('unifont', 16).render(repr(tela), False, (255, 255, 255))
 		txt = pygame.transform.scale(txt, [4 * i for i in txt.get_rect()][2:])
 		print tela
 
