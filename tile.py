e291e1af6a6633a68f163f53c66ea938604cace8
Rodrigo Exterckötter Tjäder <rodrigo@tjader.com.br>
2011-06-01 01:20:26 -0300

    Alfa

--- tile.py
+++ tile.py
@@ -9,7 +9,7 @@ class Tile:
 		self.pos = pos
 		self.size = size
 		ladrilhorect = self.sprite.get_rect()
-		self.spritemesmo = pygame.Surface((self.size[0] * ladrilhorect[2], self.size[1] * ladrilhorect[3]))
+		self.spritemesmo = pygame.Surface((self.size[0] * ladrilhorect[2], self.size[1] * ladrilhorect[3])).convert_alpha()
 		for i in range(size[0]):
 			for j in range(size[1]):
 				self.spritemesmo.blit(self.sprite, (i * ladrilhorect[2], j * ladrilhorect[3]))
