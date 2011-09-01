9be1feaa1ca2cc949feafc9e6122fce02741292f
Rodrigo Exterckötter Tjäder <rodrigo@tjader.com.br>
2011-09-01 01:50:44 -0300

    Agora com o collision.py

---
+++ collision.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+import os, pygame, geral
+
+class Collision:
+	def __init__(self, pos, colisao):
+		self.pos = pos
+		self.colisao = colisao
+
+	@staticmethod
+	def XML(node):
+		x = int(node.getAttribute('x'))
+		y = int(node.getAttribute('y'))
+		colisao = [int(i) for i in node.getAttribute('colisao').split(',')]
+		collision = Collision((x, y), colisao)
+		return collision
+
+	def get_rect(self):
+		return [i / geral.px for i in self.sprites[0].get_rect()]
+
